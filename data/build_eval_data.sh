7caa245f97bbbd2de072a4873611fb86b2bab417
Sriram Rao <sriramsrao@gmail.com>
2021-01-25 15:15:55 -0800

    Make output dir before running script.

--- data/build_eval_data.sh
+++ data/build_eval_data.sh
@@ -1,6 +1,7 @@
 #!bash
 for i in 14
 do
+   mkdir -p TFRecords/predict/sec$i
    python process_new_data.py --catalog ../catalog/period_info-sec$i.csv \
         --tic2fileDB ../catalog/tic2file-sec$i.csv \
         --input ../lcs/sector$i/ --output TFRecords/predict/sec$i/
